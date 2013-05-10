a50b4b8f8b9a79d2a0e85f0a531bbf4aeb421014
OpenShift guest <thatitninja@hotmail.com>
2013-05-10 18:49:21 -0400

    init

---
+++ dashboardproject/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dashboardproject::Application.config.secret_token = '7e82fc050a8a98e517082484a17a19bcca713a7b20264a44361bd10743748374986df77b7ddc962045bfcadecffa8592280b8eb9ef5228235a8b0db4432488b4'
